3a59a62f66fe18237cb31d09be0cbbe522d9484f
Ramiro Olivencia <32349705+rolivencia@users.noreply.github.com>
2021-01-24 03:50:10 -0300

    Tooling: Setup of build and deployment process

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -7,7 +7,7 @@ import { join } from 'path';
 @Module({
   imports: [
     ServeStaticModule.forRoot({
-      rootPath: join(__dirname, '..', 'front'),
+      rootPath: join(__dirname, '.', 'client-web'),
     }),
   ],
   controllers: [AppController],
