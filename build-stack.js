fac200c8987837c0977535d55f3dc4608daf55d7
Ramiro Olivencia <32349705+rolivencia@users.noreply.github.com>
2021-01-24 06:27:18 -0300

    Tooling: Fixed "heroku-cleanup" script to deploy on Heroku

---
+++ build-stack.js
@@ -0,0 +1,12 @@
+// mv client-web/dist/* server/dist/client-web
+const copyfiles = require('copyfiles');
+
+let source = 'client-web/dist/**/*';
+let destination = 'server/dist/client-web';
+
+copyfiles([source, destination], 2, (c, err) => {
+    console.log('Copying Angular client files to generated NestJS dist bundle...');
+    if (err) {
+        console.log(err);
+    }
+});
