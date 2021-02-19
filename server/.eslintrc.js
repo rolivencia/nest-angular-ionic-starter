cddd99f2d7db7231d9d21e8343310df2f2caa5ca
Ramiro Olivencia <32349705+rolivencia@users.noreply.github.com>
2021-02-19 00:20:10 -0300

    Added eslint configuration and missing plugins

--- server/.eslintrc.js
+++ server/.eslintrc.js
@@ -1,26 +1,42 @@
 module.exports = {
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    project: 'tsconfig.json',
-    sourceType: 'module',
-  },
-  plugins: ['@typescript-eslint/eslint-plugin', 'rxjs'],
-  extends: [
-    'plugin:@typescript-eslint/recommended',
-    "plugin:rxjs/recommended",
-    'prettier/@typescript-eslint',
-    'plugin:prettier/recommended',
-  ],
-  root: true,
-  env: {
-    node: true,
-    jest: true,
-  },
-  ignorePatterns: ['.eslintrc.js'],
-  rules: {
-    '@typescript-eslint/explicit-function-return-type': 'error',
-    '@typescript-eslint/explicit-module-boundary-types': 'error',
-    '@typescript-eslint/no-explicit-any': 'error',
-    'rxjs/finnish': 'error'
-  },
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+        project: 'tsconfig.json',
+        sourceType: 'module',
+    },
+    plugins: ['@typescript-eslint/eslint-plugin', 'rxjs'],
+    extends: [
+        'plugin:@typescript-eslint/recommended',
+        'plugin:rxjs/recommended',
+        'prettier/@typescript-eslint',
+        'plugin:prettier/recommended',
+    ],
+    root: true,
+    env: {
+        node: true,
+        jest: true,
+    },
+    ignorePatterns: ['.eslintrc.js'],
+    rules: {
+        '@typescript-eslint/explicit-function-return-type': 'error',
+        '@typescript-eslint/explicit-module-boundary-types': 'error',
+        '@typescript-eslint/no-explicit-any': 'error',
+        '@typescript-eslint/no-empty-function': ['error', { allow: ['constructors'] }],
+        'rxjs/finnish': [
+            'error',
+            {
+                functions: false,
+                methods: false,
+                names: {
+                    '^(canActivate|canActivateChild|canDeactivate|canLoad|intercept|resolve|validate)$': false,
+                },
+                parameters: false,
+                properties: true,
+                types: {
+                    '^EventEmitter$': false,
+                },
+                variables: true,
+            },
+        ],
+    },
 };
