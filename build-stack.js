255faa79dc55659d382f0892edc3810e8fa9ce63
Ramiro Olivencia <32349705+rolivencia@users.noreply.github.com>
2021-03-24 01:52:12 -0300

    Served client app and admin separately. Adapted admin Angular app routing to support subroute

--- build-stack.js
+++ build-stack.js
@@ -2,12 +2,22 @@
 const copyfiles = require("copyfiles");
 
 let source = "client-web/dist/**/*";
-let destination = "server/dist/client-web";
+let destination = "server/dist/admin";
 
 copyfiles([source, destination], 2, (c, err) => {
   console.log(
     "Copying Angular client files to generated NestJS dist bundle..."
   );
+  if (err) {
+    console.log(err);
+  }
+});
+
+source = "client-app/www/**/*";
+destination = "server/dist/app";
+
+copyfiles([source, destination], 2, (c, err) => {
+  console.log("Copying Ionic app files to generated NestJS dist bundle...");
   if (err) {
     console.log(err);
   }
